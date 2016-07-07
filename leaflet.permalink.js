37220bdb30a1ee69c941e8cc957259e1978a1b72
MarcChasse <marcchasse@gmail.com>
2016-07-07 11:55:01 -0600

    Almost there? error in addHooks on this._map.on(...)

--- leaflet.permalink.js
+++ leaflet.permalink.js
@@ -1,56 +1,68 @@
-L.Permalink = {
-    //gets the map center, zoom-level and rotation from the URL if present, else uses default values
-    getMapLocation: function (zoom, center) {
-        'use strict';
-        zoom = (zoom || zoom === 0) ? zoom : 18;
-        center = (center) ? center : [52.26869, -113.81034];
+L.Map.mergeOptions({
+	permalink: false
+});
 
-        if (window.location.hash !== '') {
-            var hash = window.location.hash.replace('#', '');
-            var parts = hash.split(',');
-            if (parts.length === 3) {
-                center = {
-                    lat: parseFloat(parts[0]),
-                    lng: parseFloat(parts[1])
-                };
-                zoom = parseInt(parts[2].slice(0, -1), 10);
-            }
+L.Map.Permalink = L.Handler.extend({
+	initialize: function (map) {
+		this._map = map;
+		var mappos = this._getMapLocation(this._map.options.zoom, this._map.options.center);
+		this._map.options.zoom = mappos.zoom;
+		this._map.options.center = mappos.center;
+	},
+
+	addHooks: function () {
+		var shouldUpdate = true;
+		window.addEventListener('popstate', this._restoreViewState());
+		this._map.on('moveend', this._updatePermalink(shouldUpdate));
+	},
+
+	removeHooks: function () {
+		this._map.off('moveend', this._updatePermalink);
+		window.removeEventListener('popstate', this._restoreViewState());
+	},
+
+	_getMapLocation: function (zoom, center) {
+	    if (window.location.hash !== '') {
+	        var hash = window.location.hash.replace('#', '');
+	        var parts = hash.split(',');
+	        if (parts.length === 3) {
+	            center = {
+	                lat: parseFloat(parts[0]),
+	                lng: parseFloat(parts[1])
+	            };
+	            zoom = parseInt(parts[2].slice(0, -1), 10);
+	        }
+	    }
+	    return {zoom: zoom, center: center};
+	},
+
+	_restoreViewState: function (event) {
+        if (!event || event.state === null) {
+        	console.log('_restoreViewState returned');
+            return;
         }
-        return {zoom: zoom, center: center};
+        map.setView(event.state.center, event.state.zoom);
+        shouldUpdate = false;
     },
 
-    setup: function (map) {
-        'use strict';
-        var shouldUpdate = true;
-        var updatePermalink = function () {
-            if (!shouldUpdate) {
-                // do not update the URL when the view was changed in the 'popstate' handler (browser history navigation)
-                shouldUpdate = true;
-                return;
-            }
+	_updatePermalink: function (shouldUpdate) {
+        if (!shouldUpdate) {
+            // do not update the URL when the view was changed in the 'popstate' handler (browser history navigation)
+            shouldUpdate = true;
+            return;
+        }
 
-            var center = map.getCenter();
-            var hash = '#' +
-                    Math.round(center.lat * 100000) / 100000 + ',' +
-                    Math.round(center.lng * 100000) / 100000 + ',' +
-                    map.getZoom() + 'z';
-            var state = {
-                zoom: map.getZoom(),
-                center: center
-            };
-            window.history.pushState(state, 'map', hash);
+        var center = this._map.getCenter();
+        var hash = '#' +
+                Math.round(center.lat * 100000) / 100000 + ',' +
+                Math.round(center.lng * 100000) / 100000 + ',' +
+                this._map.getZoom() + 'z';
+        var state = {
+            zoom: this._map.getZoom(),
+            center: center
         };
-
-        map.on('moveend', updatePermalink);
-
-        // restore the view state when navigating through the history, see
-        // https://developer.mozilla.org/en-US/docs/Web/API/WindowEventHandlers/onpopstate
-        window.addEventListener('popstate', function (event) {
-            if (event.state === null) {
-                return;
-            }
-            map.setView(event.state.center, event.state.zoom);
-            shouldUpdate = false;
-        });
+        window.history.pushState(state, 'map', hash);
     }
-};
+});
+
+L.Map.addInitHook('addHandler', 'permalink', L.Map.Permalink);
